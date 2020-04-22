4bee7cf85bb7b4cdc139c06fbe48ee17747bc304
Marin <1475705241@qq.com>
2020-04-22 10:30:53 +0800

    介绍import配置，task等

---
+++ src/com/brilliance/test/ClientTest.java
@@ -0,0 +1,11 @@
+package com.brilliance.test;
+
+import com.brilliance.eibs.main.Client;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+
+		Client.main(new String[] {"test", "hello", "MarinZhang"});
+	}
+}
---
+++ src/com/brilliance/test/MsgTestUtil.java
@@ -0,0 +1,19 @@
+package com.brilliance.test;
+
+public class MsgTestUtil {
+	
+	
+	public static String handleValue(String arg)
+	{
+		//....
+		return arg.substring(1);
+	}
+	
+	public static String handleValue2(String arg)
+	{
+		//....
+		return "test";
+	}
+
+
+}
