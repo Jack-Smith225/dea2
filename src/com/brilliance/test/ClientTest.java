61fe74819e042229bfba2f749258affeff173814
Marin <1475705241@qq.com>
2020-04-27 11:36:05 +0800

    写接口平台试题 --> 完成第一题

--- src/com/brilliance/test/ClientTest.java
+++ src/com/brilliance/test/ClientTest.java
@@ -6,6 +6,6 @@ public class ClientTest {
 
 	public static void main(String[] args) {
 
-		Client.main(new String[] {"hello", "test3", "MarinZhang"});
+		Client.main(new String[] {"test_zhangyan", "test1", "MarinZhang"});
 	}
 }
