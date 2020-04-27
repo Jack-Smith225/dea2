e36386d29468e2a7bf2917d797a0eb3caa46321b
Marin <1475705241@qq.com>
2020-04-27 14:03:07 +0800

    写接口平台试题 --> 完成第4题

--- src/com/brilliance/test/MsgTestUtil.java
+++ src/com/brilliance/test/MsgTestUtil.java
@@ -1,19 +1,26 @@
 package com.brilliance.test;
 
 public class MsgTestUtil {
-	
-	
+
+
 	public static String handleValue(String arg)
 	{
 		//....
 		return arg.substring(1);
 	}
-	
+
 	public static String handleValue2(String arg)
 	{
 		//....
 		return "test";
 	}
+
+  /**
+   * 第四题
+   */
+  public static String test_zhangyan() {
+    return "我是自定义的静态函数";
+  }
 
 
 }
